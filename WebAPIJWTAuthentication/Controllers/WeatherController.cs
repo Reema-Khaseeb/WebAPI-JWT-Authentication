bc64cb0d61e24f7caf06efab960a10dde426025c
Reema Khaseeb <reemafkhaseeb@gmail.com>
2023-11-27 19:27:30 +0200

    Create the API Controller

---
+++ WebAPIJWTAuthentication/Controllers/WeatherController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace WebAPIJWTAuthentication.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WeatherController : ControllerBase
+    {
+        [AllowAnonymous]
+        [HttpGet("public")]
+        public IActionResult GetPublicData()
+        {
+            return Ok("Welcome to the public endpoint!");
+        }
+
+        [HttpGet("protected")]
+        public IActionResult GetProtectedData()
+        {
+            return Ok("Welcome to the protected endpoint!");
+        }
+    }
+}
